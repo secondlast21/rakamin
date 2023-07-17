995c4b02c962d650e03f062c03f31481bbb624ee
secondlast21 <secondlast2102@gmail.com>
2023-07-18 03:45:49 +0700

    refactor(Auth): remove unused line

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,5 +1,4 @@
 import { LoginTypes, RegisterTypes } from '../Types/auth'
-import Api from './api'
 import api from './api'
 
 export const loginService = async (body: LoginTypes): Promise<any> => await api.post('/auth/login', body)
